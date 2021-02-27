01f1f14c8a1420bac4cd76434324a0c0c634e4fa
William Miller <williamhmiller12@gmail.com>
2021-02-27 00:30:29 -0500

    Depolying?

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/track-budget_db", {
+mongoose.connect("mongodb://localhost/track-budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
